0ba2648dc6105020a684010cd68da71434a72326
AlexkononovV <alexeykononov@ua.pt>
2022-05-06 11:33:00 +0100

    cors, allows all origins

--- fastApi/app/main.py
+++ fastApi/app/main.py
@@ -15,10 +15,19 @@ from .database import SessionLocal, engine
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+from fastapi.middleware.cors import CORSMiddleware
+
 #models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 async def init_db():
     async with engine.begin() as conn:
