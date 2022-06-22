3e9daf6bd06587ba40407f5cac02ca225b000821
rodrigo <rodrigo.santos18@ua.pt>
2022-06-22 14:53:53 +0100

    authentication

--- authentication/src/init.sql
+++ authentication/src/init.sql
@@ -1,10 +1,8 @@
--- this file must be in directory ./db/entry
-
 CREATE DATABASE IF NOT EXISTS users;
 
 USE users;
 
-CREATE TABLE USERS (
+CREATE TABLE users (
 	id int AUTO_INCREMENT PRIMARY KEY,
 	email varchar(50) UNIQUE NOT NULL,
 	username varchar(70) NOT NULL, 
