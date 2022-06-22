8d9667b091c149b2411e65ff541c8116d856ad42
AlexkononovV <alexeykononov@ua.pt>
2022-06-22 09:41:31 +0100

    react k8s deployments fixed

--- stock/setup.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f deploy/egs7/postgresdeployment.yaml
-
-kubectl apply -f deploy/egs7/postgresservice.yaml
-
-kubectl apply -f deploy/egs7/stockdeployment.yaml
-
-kubectl apply -f deploy/egs7/stockservice.yaml
